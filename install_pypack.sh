fe4102c738c6368927df1ebee54e7527f2108e7f
Anh Nguyen <38659196+anhydri@users.noreply.github.com>
2018-10-11 08:27:24 +1100

    Add files via upload

---
+++ install_pypack.sh
@@ -0,0 +1,12 @@
+pip install --upgrade pip
+
+pip install numpy
+
+pip install pandas
+
+pip install plotly
+
+pip install ipywidgets
+jupyter nbextension enable --py widgetsnbextension
+
+pip install py3Dmol
